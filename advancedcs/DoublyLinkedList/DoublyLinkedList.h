6b40ded271a642af7e849bd0f19e19d5a7a5a1a1
jmora258 <jmora258@ucla.edu>
2015-04-17 12:27:26 -0700

    Rename doublyLinkedList folder to DoublyLinkedList, add DoublyLinkedList header file.

---
+++ advancedcs/DoublyLinkedList/DoublyLinkedList.h
@@ -0,0 +1,10 @@
+
+/* Author: 			Justino Mora
+ * Project: 		Doubly-Linked List 
+ * Date: 			April 17, 2015 
+ * Description: 	DoubliyLinkedList Header file 
+*/ 
+
+#ifndef _DOUBLYLINKEDLIST_H
+#define _DOUBLYLINKEDLIST_H
+
