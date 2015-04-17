5f4c763215f8356ff92e0250b2811ce6c502ca45
jmora258 <jmora258@ucla.edu>
2015-04-17 13:00:04 -0700

    Add descriptive comments about the header file's functionality and interface.

--- advancedcs/DoublyLinkedList/DoublyLinkedList.h
+++ advancedcs/DoublyLinkedList/DoublyLinkedList.h
@@ -5,6 +5,43 @@
  * Description: 	DoubliyLinkedList Header file 
 */ 
 
-#ifndef _DOUBLYLINKEDLIST_H
-#define _DOUBLYLINKEDLIST_H
+#ifndef _DOUBLY_LINKED_LIST_H
+#define _DOUBLY_LINKED_LIST_H
+
+typedef double ItemType; // will accept any type 
+
+// Linked List will be composed of Nodes, each node consists of a value 
+// and 2 pointer Nodes. The *next will point to the next Node after it, and 
+// the *prev will point to the node behind it. The first Node's *prev will 
+// point to the nullptr just as the last Node's *next pointer will point to
+// the nullptr. 
+struct Node 
+{
+	ItemType value; 
+	int count; // # of times value appears in the list 
+	Node *next;
+	Node *prev;  
+}; 
+
+
+class DoublyLinkedList 
+{
+	public: 
+		DoublyLinkedList(); 
+		~DoublyLinkedList(); 
+
+		void addItem(ItemType v); 
+		void addToFront(ItemType v);
+		void addTORear(ItemType v); 
+		
+		bool findItem(ItemType v); 
+		void deleteItem(ItemTyep v); 
+		void printItems(); // prints all of the items in the list 
+
+	private: 
+		Node* head; 
+		Node* tail; 
+};
+
+ #endif 
 
