56025c190079328537bf554ae9d2cb34b22fdc41
jmora258 <jmora258@ucla.edu>
2015-04-17 13:00:52 -0700

    Create the source file to test the DoublyLinkedList header and implementation files.

---
+++ advancedcs/DoublyLinkedList/DoublyLinkedListTester.cpp
@@ -0,0 +1,17 @@
+/* Author: 			Justino Mora
+ * Project: 		Doubly-Linked List 
+ * Date: 			April 17, 2015 
+ * Description: 	DoubliyLinkedList Tester program 
+*/ 
+
+#include "DoubliyLinkedList.h"
+
+#include <iostream> 
+#include <string> 
+
+ using namespace std; 
+
+ int main() 
+ {
+ 	cout << "Hello World!" << endl; 
+ }
