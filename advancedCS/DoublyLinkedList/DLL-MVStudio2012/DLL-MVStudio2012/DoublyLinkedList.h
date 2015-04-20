adfca2d773814475941618d65787cae88c1cc0c2
jmora258 <jmora258@ucla.edu>
2015-04-20 13:48:02 -0700

    Create Microsoft Visual Studio project to finish and test DoublyLinkedList program.

---
+++ advancedCS/DoublyLinkedList/DLL-MVStudio2012/DLL-MVStudio2012/DoublyLinkedList.h
@@ -0,0 +1,47 @@
+/* Author: 			Justino Mora
+ * Project: 		Doubly-Linked List 
+ * Date: 			April 17, 2015 
+ * Description: 	DoubliyLinkedList Header file 
+*/ 
+
+#ifndef _DOUBLY_LINKED_LIST_H
+#define _DOUBLY_LINKED_LIST_H
+
+typedef double ItemType; // will accept any type 
+
+// Linked List will be composed of Nodes, each node consists of a value 
+// and 2 pointer Nodes. The *next will point to the next Node after it, and 
+// the *prev will point to the node behind it. The first Node's *prev will 
+// point to the nullptr just as the last Node's *next pointer will point to
+// the nullptr. 
+struct Node 
+{
+	ItemType value; 
+	int count; // # of times value appears in the list 
+	Node *next;
+	Node *prev;  
+}; 
+
+
+class DoublyLinkedList 
+{
+	public: 
+		DoublyLinkedList(); 
+		DoublyLinkedList(ItemType v); // create & add 1 item
+		~DoublyLinkedList(); 
+
+		bool addItem(ItemType v); 
+		bool addToFront(ItemType v);
+		bool addToRear(ItemType v); 
+		
+		bool findItem(ItemType v); 
+		bool deleteItem(ItemTyep v); 
+		void printItems(); // prints all of the items in the list 
+
+	private: 
+		Node* head; 
+		Node* tail; 
+};
+
+ #endif 
+
---
+++ advancedCS/DoublyLinkedList/DLL-MVStudio2012/DLL-MVStudio2012/DoublyLinkedListTester.cpp
@@ -0,0 +1,45 @@
+/* Author: 			Justino Mora
+ * Project: 		Doubly-Linked List 
+ * Date: 			April 17, 2015 
+ * Description: 	DoubliyLinkedList Tester program 
+*/ 
+
+#include "DoublyLinkedList.h"
+
+#include <iostream> 
+#include <string> 
+
+ using namespace std; 
+
+ int main() 
+ {
+ 	// Variables needed to test the class:
+ 	double item = 0; 
+
+ 	cout << "==============================================" << endl; 
+ 	cout << "====== DoubliyLinkedList Tester Program ======" << endl; 
+ 	cout << endl; 
+
+ 	cout << "Test the Constructor, if succesful, then one should "
+ 		 << "be able to add an item." << endl; 
+
+ 	DoublyLinkedList myList = new DoublyLinkedList(); 
+
+ 	item = 5; 
+ 	cout << "Test addItem(ItemType v) method: add " << item 
+ 		 << " to the list!" << endl; 
+ 	if(myList.addItem(item))
+ 		cout << item << " was succesfully added to the list!" << endl; 
+ 	else 
+ 		cout << "Failed to add " << item << " to the list." << endl ;
+
+ 	item = 7; 
+ 	cout << "Test addToFront(ItemType v) method: add " << item 
+ 		 << " to the front of the list!" << endl; 
+ 	if(myList.addItem(item))
+ 		cout << item << " was succesfully added to the front " 
+ 			 << "of the list!" << endl; 
+ 	else 
+ 		cout << "Failed to add " << item << " to the front." << endl ;
+
+ }
