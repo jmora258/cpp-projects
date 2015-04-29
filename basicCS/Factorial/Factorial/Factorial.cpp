67ec4fa7ed95faec178998183e1ca3dcb884cf94
jmora258 <jmora258@ucla.edu>
2015-04-29 12:23:30 -0700

    Create empty C++ FactorialSolver program.

---
+++ basicCS/Factorial/Factorial/Factorial.cpp
@@ -0,0 +1,20 @@
+/* Author:			Justino Mora
+ * Date:			April 29, 2015 
+ * Project:			Factorial
+ * Description:		This simple program finds the factorial of a positive number 
+ *					and displays the result to the user. 
+*/ 
+#include <iostream> 
+
+using namespace std; 
+
+int main(){
+	cout << " ------------------ Factorial Solver ---------------------" << endl; 	
+
+	int num = 0; // Holds user input 
+
+
+
+}
+
+int 
